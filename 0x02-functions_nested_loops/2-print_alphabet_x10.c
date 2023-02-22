ee4364de2156a499185da1b8a5326c454d80c74c
Brian-Manga <brandmanga@gmail.com>
2023-02-22 11:12:54 -0800

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10.- Entry point
+ *Description-print 10 times the alphabet
+ *Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+	{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+	}
