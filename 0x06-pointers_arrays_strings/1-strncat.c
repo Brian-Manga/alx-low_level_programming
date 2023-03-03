8fae60b9da8b1e661e4353fab072348525bb4126
Brian-Manga <brandmanga@gmail.com>
2023-03-03 12:34:12 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncat - Concatenate two strings and don't need
+* to be null terminated
+* @dest: param pointer
+* @src: param pointer
+* @n: int param
+* Return: char pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = strlen(dest);
+
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
