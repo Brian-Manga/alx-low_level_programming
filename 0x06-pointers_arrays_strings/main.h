6a0433dca3437527088fcb14d58bc285a2f6c51d
Brian-Manga <brandmanga@gmail.com>
2023-04-05 11:16:42 -0700

    103 ifinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
 	char *leet(char *);
+	char *infinite_add(char *n1, char *n2, char *r, int size_r);
 	char *string_toupper(char *);
 	void print_number(int n);
 #endif
