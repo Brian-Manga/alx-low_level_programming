83a9807e75eefe51a5f7267e5f9ad366714eccf7
Brian-Manga <brandmanga@gmail.com>
2023-04-05 11:40:13 -0700

    104 print buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 	char *_strncat(char *dest, char *src, int n);
 	char *leet(char *);
 	char *infinite_add(char *n1, char *n2, char *r, int size_r);
+	void print_buffer(char *b, int size);
 	char *string_toupper(char *);
 	void print_number(int n);
 #endif
